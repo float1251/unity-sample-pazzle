884d15d9652d52a3b98daaa74fb3cbe67eaf25e9
float1251 <taka.05022002@gmail.com>
2014-08-23 14:52:27 +0900

    create stage and block

---
+++ Assets/Scripts/DebugScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugScript : MonoBehaviour
+{
+
+    public GameObject block;
+
+    public void FillBlock ()
+    {
+        for (int x=1; x<=10; x++) {
+            for (int y=1; y<=20; y++) {
+                Debug.Log (string.Format ("x:{0}, y:{1}", x, y));
+                var go = Instantiate (block) as GameObject;
+                go.name = Utils.GetBlockName (x, y);
+                go.transform.position = Utils.ConvertIndexToWorldPoint (x, y);
+            }
+        }
+    }
+
+    public void DeleteAllBlock ()
+    {
+        var blocks = GameObject.FindGameObjectsWithTag ("Block");
+        foreach (var block in blocks) {
+            GameObject.Destroy (block);
+        }
+    }
+}
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Utils
+{
+
+    /// <summary>
+    /// Converts the index to world point.
+    /// </summary>
+    /// <returns>The index to world point.</returns>
+    /// <param name="xIndex">X index. 1 - 10</param>
+    /// <param name="yIndex">Y index. 1 - 20</param>
+    public static Vector3 ConvertIndexToWorldPoint (int xIndex, int yIndex)
+    {
+        // when xIndex is 1, x is -4.5. xIndex is 10, 5.5
+        var x = xIndex;
+        var y = yIndex;
+        return new Vector3 (x, y, 0);
+    }
+
+    public static string GetBlockName (int xIndex, int yIndex)
+    {
+        return "Block:" + xIndex + "," + yIndex;
+    }
+
+}
